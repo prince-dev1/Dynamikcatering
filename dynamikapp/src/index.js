3f59c47ad6c92adb8aa7ba2f6774e08cf2537c7f
oloniju taiwo <olonijutaiwo@gmail.com>
2025-04-29 18:03:54 +0100

    Update Dynamikcatering.html and related files with new sections and features

--- dynamikapp/src/index.js
+++ dynamikapp/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<h1 style= {{color:'red',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    display:'flex',
+    justifyContent:'center',
+
+    overflow:'hidden', 
+    margin:'0px',
+    padding:'20px',
+    textAlign:'center',
+    fontFamily:'Helecta'}}> Welcome to Dynamik Caterings!</h1>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
